eb3ca4fa24ab1921f4233a4fb692e2531df7e985
callor <callor@callor.com>
2017-08-30 16:04:29 +0900

    2017-8-30

--- MyMusic/app/src/main/java/com/callor/mymusic/MainActivity.java
+++ MyMusic/app/src/main/java/com/callor/mymusic/MainActivity.java
@@ -2,18 +2,24 @@ package com.callor.mymusic;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>  {
+
+    RecyclerView recyclerView = null;
+    MusicAdapter musicAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,21 +28,74 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // 음악 미디어 List를 읽어
+        // EXTERNAL STORAGE
         // LOLLIPOP 이상에서는 manifest와 관계없이 별도로 권한을 승인받아야 하는데
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ) {
             // EXTERNAL Storage 읽기 권한이 없으면
             if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
+                String[] strReq = new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        "음악을 읽기 위한 권한 설정"
+                };
                 // 권한 승인창 띄우기
-                ActivityCompat.requestPermissions(
-                        this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1000);
+                //                              context, 메시지, key
+                ActivityCompat.requestPermissions(this,strReq,1000);
 
             } else {
-//                getAu
             }
         }
 
+        // 권한 확인 끝나면 Adapter recycler view와 연결
+        recyclerView = (RecyclerView)findViewById(R.id.music_list);
+        musicAdapter = new MusicAdapter(this,null);
+        recyclerView.setAdapter(musicAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        getSupportLoaderManager().initLoader(1, null, this);
 
     }
+
+    // 내장 메모리로부터 데이터(음악)를 읽을때,
+    // 비동기 방식으로 읽기위한 기능들
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI ; // 음악리스트가 저장된 STORAGE 경로
+
+        // MediaStore에 있는 음악리스정보 DB중에서 아래 항목들을 리스트로 보고 싶다.
+        String[] projection = new String[] {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.ALBUM,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA
+        };
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " = 1 " ; // WHERE 음악파일인 것들만
+        String sortOrder = MediaStore.Audio.Media.TITLE + " COLLATE LOCALIZED ASC "; // 한글 어순의 오름차순 정렬
+        Loader loader = new CursorLoader(
+                getApplicationContext(),
+                uri,
+                projection,
+                selection,
+                null
+                ,sortOrder
+                );
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        musicAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        musicAdapter.swapCursor(null);
+    }
+
 }
--- MyMusic/app/src/main/java/com/callor/mymusic/MusicAdapter.java
+++ MyMusic/app/src/main/java/com/callor/mymusic/MusicAdapter.java
@@ -1,9 +1,15 @@
 package com.callor.mymusic;
 
 
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -15,20 +21,79 @@ import com.squareup.picasso.Picasso;
  * Created by callor on 2017-08-29.
  */
 
-public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder> {
-    @Override
-    public MusicHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class MusicAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Cursor cursor;
+    private DataSetObserver dataSetObserver ;
+
+    private boolean dataVaid ;
+    private int rowIdColumn ;
+
+    public MusicAdapter(Context context, Cursor cursor) {
+
+        this.cursor = cursor;
+        // Data Load(읽기)되기전 생성된 adapter는
+        // cursor가 null 인상태인데 그 상태를 알려주는 flag
+        this.dataVaid = cursor != null;
+        this.rowIdColumn = this.dataVaid ? this.cursor.getColumnIndex("_id") : -1;
+
+        // load와 cursor data를 연동시키는 부분
+        this.dataSetObserver = new NotifyingDataSetObserver();
+        if(this.cursor != null) {
+            this.cursor.registerDataSetObserver(this.dataSetObserver);
+        }
+
     }
 
     @Override
-    public void onBindViewHolder(MusicHolder holder, int position) {
+    public MusicHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.music_item,parent,false);
+        return new MusicHolder(v);
+    }
 
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        cursor.moveToPosition(position);
+        ((MusicHolder)holder).bindCursor(cursor);
     }
 
     @Override
     public int getItemCount() {
+        if(dataVaid && cursor != null) {
+            return cursor.getCount();
+        }
         return 0;
+    }
+
+    // 새로 데이터를 Loading하거나
+    // 데이터 갱신되면
+    // 기존 화면에 있는 cursor와 변경된 cursor를 바꾸어서
+    // 화면을 갱신하도록 하는 method
+    public Cursor swapCursor(Cursor newCursor) {
+
+        if(newCursor == cursor) {
+            return null;
+        }
+        final Cursor oldCursor = cursor;
+        if(oldCursor != null && dataSetObserver != null) {
+            oldCursor.unregisterDataSetObserver(dataSetObserver);
+        }
+
+        cursor = newCursor ;
+        if(cursor != null) {
+            if(dataSetObserver != null) {
+                cursor.registerDataSetObserver(dataSetObserver);
+            }
+            rowIdColumn = newCursor.getColumnIndexOrThrow("_id");
+            dataVaid = true;
+            notifyDataSetChanged();
+        } else {
+            rowIdColumn = -1;
+            dataVaid = false;
+            notifyDataSetChanged();
+        }
+        return oldCursor;
     }
 
     public class MusicHolder extends RecyclerView.ViewHolder {
@@ -48,18 +113,48 @@ public class MusicAdapter extends RecyclerView.Adapter<MusicAdapter.MusicHolder>
             txt_duration = (TextView)itemView.findViewById(R.id.txt_item_duration);
         }
 
-        public void setMusicItem(MusicVO item, int postion) {
+        public void bindCursor(Cursor cursor) {
+            final Uri artUri = Uri.parse("content://media/external/audio/albumart");
 
-            txt_title.setText(item.mTitle);
-            txt_sub_title.setText(item.mArtist + "(" + item.mAlbum + ")"  );
-            txt_duration.setText(DateFormat.format("mm:ss", item.mDuration ));
-            Picasso
-                    .with(itemView.getContext())
-                    .load(artUri)
+            int cIndex = cursor.getColumnIndex(MediaStore.Audio.AudioColumns.TITLE);
+            String strTitle = cursor.getString(cIndex);
+            txt_title.setText(strTitle);
+
+            cIndex = cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ARTIST);
+            String subTitle = cursor.getString(cIndex);
+
+            cIndex = cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ALBUM);
+            subTitle += "(" + cursor.getString(cIndex) + ")";
+            txt_sub_title.setText(subTitle);
+
+            cIndex = cursor.getColumnIndex(MediaStore.Audio.AudioColumns.DURATION);
+            long duration = cursor.getLong(cIndex);
+            txt_duration.setText(DateFormat.format("mm:ss",duration)); // 03:00
+
+            cIndex = cursor.getColumnIndex(MediaStore.Audio.AudioColumns.ALBUM_ID);
+            // 앨범사진정보를 가져오기 위한 앨범 ID 추출
+            long albumId = cursor.getLong(cIndex);
+            // 사진정보 가져오기
+            Uri albumArtUri = ContentUris.withAppendedId(artUri,albumId);
+            Picasso.with(itemView.getContext())
+                    .load(albumArtUri)
                     .error(R.drawable.ic_music_note_black_24dp)
                     .into(imgAlbumArt);
-
+        }
+    }
+    private class NotifyingDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            dataVaid = true;
+            notifyDataSetChanged();
         }
 
+        @Override
+        public void onInvalidated() {
+            super.onInvalidated();
+            dataVaid = false;
+            notifyDataSetChanged();
+        }
     }
 }
